c05ec7670644d15fe18f262d06a4d7ed5af91388
shengjiew2 <shengjiew2@student.unimelb.edu.au>
2017-02-17 17:29:16 +1100

    Add files via upload

---
+++ Character/CameraController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public float lookSmooth = 0.1f;
+    public Vector3 offsetFromTarget = new Vector3(0, 3, -4);
+
+    public float xTilt = 10; // How far I want my camera on the x-axis, look downward at our target.(3 person perspective)
+
+    Vector3 destination = Vector3.zero;
+    CharacterController charController;
+    float rotateVel = 0;
+
+	// Use this for initialization
+	void Start () {
+        SetCemaraTarget(target);
+	}
+	void SetCemaraTarget(Transform t)
+    {
+        target = t;
+        
+        if (target != null)
+        {
+            if (target.GetComponentInChildren<CharacterController>())
+            {
+                charController = target.GetComponentInChildren<CharacterController>();
+            }
+            else
+                Debug.LogError("Char need a Char Controller");
+        }
+        else
+            Debug.LogError("Sorry, There is No target for camera.");
+    }
+	// Update is called once per frame
+    //This updata later basic on character's update
+	void LateUpdate () {
+        //Moving
+        MoveToTarget();
+
+        //Rotating
+        LookAtTarget();
+	}
+    void MoveToTarget()
+    {
+        destination = charController.TargetRotation * offsetFromTarget;
+        destination += target.position;
+        transform.position = destination;
+    }
+    void LookAtTarget()
+    {
+        float eulerYAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, target.eulerAngles.y, ref rotateVel, lookSmooth);
+        transform.rotation = Quaternion.Euler(transform.eulerAngles.x, eulerYAngle, 0);
+    }
+
+}
---
+++ Character/CharacterController.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+    public float inputDelay = 0.1f;
+    public float forwardVel = 12;
+    public float rotateVel = 100;
+
+    Quaternion targetRotation;
+    Rigidbody rBody;
+    float forwardInput, turnInput;
+    
+    public Quaternion TargetRotation
+    {
+        get { return targetRotation; }
+    }
+	// Use this for initialization
+	void Start () {
+        targetRotation = transform.rotation;
+        if (GetComponent<Rigidbody>())
+            rBody = GetComponent<Rigidbody>();
+        else
+            Debug.LogError("Sorry, I need a RigidBody.");
+
+        forwardInput = 0;
+        turnInput = 0;
+	}
+
+    void GetInput()
+    {
+        forwardInput = Input.GetAxis("Vertical"); //This will give a value between -1 to 1 and control our player forward and backwardsc
+        Debug.Log(forwardInput);
+        turnInput = Input.GetAxis("Horizontal");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        GetInput();
+        Turn();
+	}
+    void FixedUpdate()
+    {
+        Run();
+    }
+    void Run()
+    {
+        if (Mathf.Abs(forwardInput) > inputDelay)
+        
+            rBody.velocity = transform.forward * forwardInput * forwardVel;
+        
+        else
+            rBody.velocity = Vector3.zero;
+
+
+
+    }
+    void Turn()
+    {
+        if (Mathf.Abs(turnInput) > inputDelay)
+        {
+            targetRotation *= Quaternion.AngleAxis(rotateVel * turnInput * Time.deltaTime, Vector3.up);
+        }
+        transform.rotation = targetRotation;
+    }
+}
